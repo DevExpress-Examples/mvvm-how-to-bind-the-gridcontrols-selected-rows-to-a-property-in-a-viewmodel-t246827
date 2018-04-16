9e6eb56acfa439110b9a8df4306389976578a65b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ CS/GridMVVMSelection/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+using Model;
+
+namespace SLGridMVVMSelection {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            DataContext = new ViewModel();
+        }
+    }
+}
---
+++ VB/GridMVVMSelection/MainPage.xaml.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Windows.Controls
+Imports Model
+
+Namespace SLGridMVVMSelection
+    Partial Public Class MainPage
+        Inherits UserControl
+
+        Public Sub New()
+            InitializeComponent()
+            DataContext = New ViewModel()
+        End Sub
+    End Class
+End Namespace
